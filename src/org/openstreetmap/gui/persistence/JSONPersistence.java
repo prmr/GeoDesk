a4b97d65a121486d7fc37ac5b0a80d6ed9c43e9b
Reid Holmes (MBA) <rtholmes@gmail.com>
2014-12-15 07:32:03 +0100

    Validate that JSON file contains expected element (avoids NullPointerException).

--- src/org/openstreetmap/gui/persistence/JSONPersistence.java
+++ src/org/openstreetmap/gui/persistence/JSONPersistence.java
@@ -1,22 +1,19 @@
 /*******************************************************************************
  * GeoDesk - Desktop application to view and edit geographic markers
- *
- *     Copyright (C) 2014 Martin P. Robillard, Jan Peter Stotz, and others
- *     
- *     See: http://martinrobillard.com/geodesk
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) 2014 Martin P. Robillard, Jan Peter Stotz, and others
+ * 
+ * See: http://martinrobillard.com/geodesk
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 package org.openstreetmap.gui.persistence;
 
@@ -35,9 +32,8 @@ import org.openstreetmap.gui.jmapviewer.MapMarker;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
 
 /**
- * Single point of access for reading and writing marker
- * data stored in a JSON file. The file follows the 
- * GeoJSON specification and stores a single GeometryCollection.
+ * Single point of access for reading and writing marker data stored in a JSON file. The file follows the GeoJSON
+ * specification and stores a single GeometryCollection.
  * 
  * @see http://geojson.org
  * 
@@ -45,15 +41,20 @@ import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
  */
 public final class JSONPersistence
 {
-	private JSONPersistence() {}
-	
+	private JSONPersistence()
+	{
+	}
+
 	/**
-     * Loads the marker data from file.
-     * @param pFineName The name of the source file.
-     * @return An array of MarkerData loaded from file.
-     * @throws PersistenceException If there's any problem with the load. 
-     */
-    public static MarkerData[] loadMarkers(String pFineName)
+	 * Loads the marker data from file.
+	 * 
+	 * @param pFineName
+	 *            The name of the source file.
+	 * @return An array of MarkerData loaded from file.
+	 * @throws PersistenceException
+	 *             If there's any problem with the load.
+	 */
+	public static MarkerData[] loadMarkers(String pFineName)
     {
     	try
     	{
@@ -64,11 +65,13 @@ public final class JSONPersistence
     			buffer.append(string.trim());
     		}
     		JSONObject geometryCollection = new JSONObject(buffer.toString());
-    		JSONArray points = geometryCollection.getJSONArray("geometries");
-    		MarkerData[] markers = new MarkerData[points.length()];
-    		for( int i = 0; i < markers.length; i++ )
-    		{
-    			markers[i] = extractMarker(points.getJSONObject(i));
+    		if (geometryCollection.has("geometries")) {
+    			JSONArray points = geometryCollection.getJSONArray("geometries");
+    			MarkerData[] markers = new MarkerData[points.length()];
+    			for( int i = 0; i < markers.length; i++ )
+    			{
+    				markers[i] = extractMarker(points.getJSONObject(i));
+    			}
     		}
     		return markers;
     	}
@@ -77,95 +80,100 @@ public final class JSONPersistence
     		throw new PersistenceException(exception);
     	}
     }
-    
-    private static MarkerData extractMarker(JSONObject pPoint)
-    {
-    	MarkerData lReturn = new MarkerData();
-    	JSONArray coordinates = pPoint.getJSONArray("coordinates");
-    	lReturn.aLongitude = coordinates.getDouble(0);
-    	lReturn.aLatitude = coordinates.getDouble(1);
-    	lReturn.aName = pPoint.getJSONObject("properties").getString("name");
-    	lReturn.aDescription = pPoint.getJSONObject("properties").getString("description");
-    	return lReturn;
-    }
-    
-    /**
-     * Stores the markers to disk.
-     * 
-     * @param pMarkers The marker data to persist.
-     * @param pFileName The destination file.
-     * @throws PersistenceException is there's any issue saving the markers.
-     */
-    public static void storeMarkers(MapMarker[] pMarkers, String pFileName)
-    {
-    	JSONObject geographyCollection = new JSONObject();
-    	geographyCollection.put("type", "GeometryCollection");
-    	JSONObject versionInfo = new JSONObject();
-    	versionInfo.put("geodesk-version", Version.instance().toString());
-    	geographyCollection.put("properties", versionInfo);
-    	JSONArray geometries = new JSONArray();
-    	for( MapMarker marker : pMarkers )
-    	{
-    		geometries.put(createPointObject(marker));
-    	}
-    	geographyCollection.put("geometries", geometries);
-    	List<String> out = new ArrayList<String>();
-    	out.add(geographyCollection.toString(3));
-    	try
-    	{
-    		Files.write(Paths.get(pFileName), out, StandardCharsets.ISO_8859_1);
-    	}
-    	catch( IOException exception )
-    	{
-    		throw new PersistenceException(exception);
-    	}
-    }
-    
-    private static JSONObject createPointObject(MapMarker pMarker)
-    {
-    	JSONObject lReturn = new JSONObject();
-    	lReturn.put("type", "Point");
-    	JSONArray coordinates = new JSONArray();
-    	coordinates.put(pMarker.getLongitude());
-    	coordinates.put(pMarker.getLatitude());
-    	lReturn.put("coordinates", coordinates);
-    	JSONObject properties = new JSONObject();
-    	properties.put("name", pMarker.getName());
-    	properties.put("description", pMarker.getDescription());
-    	lReturn.put("properties", properties);
-    	return lReturn;
-    }
-    
-    /**
-     * Make a backup of pFileName by copying it to the same directory,
-     * overriding any file of the same name. The backup has the same 
-     * name as the original file, with the extension .backup.
-     * @param pFileName The name of the file to back up.
-     * @throws PersistenceException If the backup is not successful.
-     */
-    public static void backup(String pFileName)
-    {
-    	try
+
+	private static MarkerData extractMarker(JSONObject pPoint)
+	{
+		MarkerData lReturn = new MarkerData();
+		JSONArray coordinates = pPoint.getJSONArray("coordinates");
+		lReturn.aLongitude = coordinates.getDouble(0);
+		lReturn.aLatitude = coordinates.getDouble(1);
+		lReturn.aName = pPoint.getJSONObject("properties").getString("name");
+		lReturn.aDescription = pPoint.getJSONObject("properties").getString("description");
+		return lReturn;
+	}
+
+	/**
+	 * Stores the markers to disk.
+	 * 
+	 * @param pMarkers
+	 *            The marker data to persist.
+	 * @param pFileName
+	 *            The destination file.
+	 * @throws PersistenceException
+	 *             is there's any issue saving the markers.
+	 */
+	public static void storeMarkers(MapMarker[] pMarkers, String pFileName)
+	{
+		JSONObject geographyCollection = new JSONObject();
+		geographyCollection.put("type", "GeometryCollection");
+		JSONObject versionInfo = new JSONObject();
+		versionInfo.put("geodesk-version", Version.instance().toString());
+		geographyCollection.put("properties", versionInfo);
+		JSONArray geometries = new JSONArray();
+		for (MapMarker marker : pMarkers)
 		{
-			Files.copy(Paths.get(pFileName), Paths.get(pFileName + ".backup"), 
-					StandardCopyOption.COPY_ATTRIBUTES,
-					StandardCopyOption.REPLACE_EXISTING);
+			geometries.put(createPointObject(marker));
 		}
-		catch( IOException exception )
+		geographyCollection.put("geometries", geometries);
+		List<String> out = new ArrayList<String>();
+		out.add(geographyCollection.toString(3));
+		try
+		{
+			Files.write(Paths.get(pFileName), out, StandardCharsets.ISO_8859_1);
+		}
+		catch (IOException exception)
 		{
 			throw new PersistenceException(exception);
 		}
-    }
-    
-    public static void main(String[] args)
+	}
+
+	private static JSONObject createPointObject(MapMarker pMarker)
 	{
-    	MarkerData[] data = JSONPersistence.loadMarkers("data\\TestMarkers.json");
-    	MapMarker[] converted = new MapMarker[data.length];
-    	for( int i = 0; i < data.length; i++ )
-    	{
-    		converted[i] = new MapMarkerDot(data[i].getLatitude(), data[i].getLongitude(), data[i].getName(), data[i].getDescription());
-    	}
-    	JSONPersistence.storeMarkers(converted, "data\\TestMarkers-out.json");
-    	
+		JSONObject lReturn = new JSONObject();
+		lReturn.put("type", "Point");
+		JSONArray coordinates = new JSONArray();
+		coordinates.put(pMarker.getLongitude());
+		coordinates.put(pMarker.getLatitude());
+		lReturn.put("coordinates", coordinates);
+		JSONObject properties = new JSONObject();
+		properties.put("name", pMarker.getName());
+		properties.put("description", pMarker.getDescription());
+		lReturn.put("properties", properties);
+		return lReturn;
+	}
+
+	/**
+	 * Make a backup of pFileName by copying it to the same directory, overriding any file of the same name. The backup
+	 * has the same name as the original file, with the extension .backup.
+	 * 
+	 * @param pFileName
+	 *            The name of the file to back up.
+	 * @throws PersistenceException
+	 *             If the backup is not successful.
+	 */
+	public static void backup(String pFileName)
+	{
+		try
+		{
+			Files.copy(Paths.get(pFileName), Paths.get(pFileName + ".backup"), StandardCopyOption.COPY_ATTRIBUTES,
+					StandardCopyOption.REPLACE_EXISTING);
+		}
+		catch (IOException exception)
+		{
+			throw new PersistenceException(exception);
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		MarkerData[] data = JSONPersistence.loadMarkers("data\\TestMarkers.json");
+		MapMarker[] converted = new MapMarker[data.length];
+		for (int i = 0; i < data.length; i++)
+		{
+			converted[i] = new MapMarkerDot(data[i].getLatitude(), data[i].getLongitude(), data[i].getName(),
+					data[i].getDescription());
+		}
+		JSONPersistence.storeMarkers(converted, "data\\TestMarkers-out.json");
+
 	}
 }
